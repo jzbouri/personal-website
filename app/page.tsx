1eeafe9553de68e890a991a2ddae82ae586bbff4
Jalal Bouri <jalal@hiddenharvestventures.com>
2025-08-26 01:31:06 -0400

    add text type component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+import TextType from "@/components/reactbits/TextType";
+
 export default function Home() {
   return (
-    <p>Coming soon...</p>
+    <TextType
+      text={[
+        "Hello there...",
+        "I'm Jalal Bouri, a software engineer",
+        "This website is a work in progress...",
+        "Come back later please",
+      ]}
+      className="text-4xl font-bold"
+      pauseDuration={1500}
+      typingSpeed={100}
+      loop={true}
+    />
   );
 }
