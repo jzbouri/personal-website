27042de988278542ca42709939491255c5a5d924
Jalal Bouri <jalal@hiddenharvestventures.com>
2025-09-07 20:54:49 -0400

    add favicon, add some scraping info

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,20 @@ const cousine = Cousine({
 });
 
 export const metadata: Metadata = {
-  title: "Jalal's Site",
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"),
+  title: "Jalal Bouri",
   description: "Jalal Bouri's personal website",
+  openGraph: {
+    title: "Jalal Bouri",
+    description: "Jalal Bouri's personal website",
+    url: "/",
+    siteName: "Jalal Bouri",
+    images: [
+      "/icon.png",
+    ],
+    type: "website",
+    locale: "en_US",
+  },
 };
 
 export default function RootLayout({
